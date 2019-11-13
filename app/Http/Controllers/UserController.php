1f2a8ed7ea02b76c4588a4e7f9983bc329accf2e
Milena <beki.malena@gmail.com>
2019-11-13 14:42:32 +0100

    User request added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use JWTAuth;
@@ -11,10 +12,10 @@ use Tymon\JWTAuth\Exceptions\JWTException;
 
 class UserController extends Controller
 {
-    public function authenticate(Request $request)
+    public function authenticate(UserRequest $request)
     {
         $credentials = $request->only('email', 'password');
-
+      
         try {
             if (! $token = JWTAuth::attempt($credentials)) {
                 return response()->json(['error' => 'invalid_credentials'], 400);
@@ -26,16 +27,13 @@ class UserController extends Controller
         return response()->json(compact('token'));
     }
 
-    public function register(Request $request)
+    public function register(UserRequest $request)
     {
-            $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
-        ]);
+      
+        $validated = $request->validated();
 
-        if($validator->fails()){
-                return response()->json($validator->errors()->toJson(), 400);
+        if(!$validated ){
+            return response()->json($request->message(), 400);
         }
 
         $user = User::create([
---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        
+        return [
+            'name' => 'required|unique:users|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Name is required',
+            'email.required'  => 'Email is required',
+            'name.unique'  => 'Name must be unique',
+            'email.unique'  => 'Email must be unique',
+            'password.required' => 'Password is required'
+        ];
+    }
+}
